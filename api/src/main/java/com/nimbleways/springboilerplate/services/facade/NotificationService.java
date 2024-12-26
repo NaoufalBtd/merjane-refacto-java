e4aa66fb6f1cd212494f16b36070b31b0821dece
Boutadarhart Naoufal <naoufel.info3@gmail.com>
2024-12-26 17:51:27 +0100

    chore: implement the notification facade contract

--- api/src/main/java/com/nimbleways/springboilerplate/services/facade/NotificationService.java
+++ api/src/main/java/com/nimbleways/springboilerplate/services/facade/NotificationService.java
@@ -1,4 +1,12 @@
 package com.nimbleways.springboilerplate.services.facade;
 
+import java.time.LocalDate;
+
 public interface NotificationService {
+
+    void sendDelayNotification(int leadTime, String productName);
+
+    void sendOutOfStockNotification(String productName);
+
+    void sendExpirationNotification(String productName, LocalDate expiryDate);
 }
